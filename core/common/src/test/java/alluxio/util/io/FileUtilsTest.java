237befce3b35e93fa21acac6411db9ec6af5573e
Alex-Lian <xinyulianuiuc@gmail.com>
2023-11-06 16:15:27 -0600

    instrument on test to show that the injection is successful

--- core/common/src/test/java/alluxio/util/io/FileUtilsTest.java
+++ core/common/src/test/java/alluxio/util/io/FileUtilsTest.java
@@ -23,11 +23,14 @@ import alluxio.conf.PropertyKey;
 import alluxio.exception.runtime.NotFoundRuntimeException;
 import alluxio.exception.runtime.UnknownRuntimeException;
 
+import edu.illinois.CTestClass;
+import edu.illinois.CTestJUnit4Runner;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -49,6 +52,8 @@ import javax.annotation.Nullable;
 /**
  * Tests for the {@link FileUtils} class.
  */
+@RunWith(CTestJUnit4Runner.class)
+@CTestClass()
 public class FileUtilsTest {
 
   private String mWorkerDataFolderPerms = Configuration.global()
